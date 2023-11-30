2e7770264cfb83a070d814b74fed9c953f4fd038
Samuel Baran <samuel.baran98@gmail.com>
2023-11-30 15:07:35 +0100

    add pictures

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -1,50 +1,113 @@
+'use client'
+import {  useEffect, useRef, } from "react";
+import Image from "next/image";
+import React from "react";
+import ugly1 from '../../../../public/ugly-1.svg'
+import ugly2 from '../../../../public/ugly-2.svg'
+import pretty1 from '../../../../public/pretty-1.svg'
+import pretty2 from '../../../../public/pretty-2.svg'
+
+
+
 export const Carousel = () => {
-  const elements = [1, 2, 3, 4, 5, 6, 7];
+  const elements = [{
+    ugly: ugly1,
+    pretty: pretty1
+  }, {
+    ugly: ugly2,
+    pretty: pretty2
+  },3,4];
+  const elementRefs = useRef<any[]>(elements.map(() => React.createRef()));
+  const dividerRef = useRef<HTMLDivElement>(null)
+  const [showDivider, setShowDivider] = React.useState(false)
 
-  const renderUgly = () => elements.map((element, index) => {
+
+  const calculateDividerOverlap = () => {
+    if(!dividerRef.current) return false
+    const divider = dividerRef.current
+    const dividerRect = divider?.getBoundingClientRect()
+    const uglyElements = document.querySelectorAll('#ugly-element')
+    const uglyElementsRects = Array.from(uglyElements).map(element => element.getBoundingClientRect())
+    const uglyElementsOverlap = uglyElementsRects.some(element => {
+      const overlap = element.left + 5  < dividerRect.left + dividerRect.width && element.left + element.width > dividerRect.left
+      return overlap
+    })
+    return uglyElementsOverlap
+  }
+
+  useEffect(() => {
+    let frame = 0
+    function overlapCheck(){
+      const dividerVisible = calculateDividerOverlap()
+      setShowDivider(dividerVisible)
+      frame = requestAnimationFrame(overlapCheck)
+    }
+
+    overlapCheck()
+
+    return () => cancelAnimationFrame(frame)
+  })
+
+  const renderUgly = () => elements.map((element:any, index) => {
+
     return (
       <div
-        key={index}
-        className="h-[300px] w-[400px] flex-shrink-0 bg-black rounded-md"
-      ></div>
+      id='ugly-element'
+      key={index}
+      ref={elementRefs.current[index]}
+      className="relative h-[250px] w-[400px]  bg-black rounded-md"
+    >
+{      element.ugly && <Image fill src={element.ugly} alt="ugly" />}
+    </div>
     );
   })
 
-  const renderPretty = () => elements.map((element, index) => {
+  const renderPretty = () => elements.map((element:any, index) => {
     return (
       <div
+      id='pretty-element'
+      ref={elementRefs.current[index]}
         key={index}
-        className="h-[300px] w-[400px] flex-shrink-0 bg-purple rounded-md"
-      ></div>
+        className="relative h-[250px] w-[400px] bg-purple rounded-md"
+      >
+      { element.pretty && <Image src={element.pretty} alt="pretty" fill />}
+
+      </div>
     );
   })
-
+  
   return (
     <section className="my-28 h-[50vh] w-full flex flex-col items-center">
       <span className=" tracking-[.25rem] w-full whitespace-nowrap self-center text-center  sm:text-decorText text-base  font-semibold  text-transparent bg-clip-text bg-gradient-to-b from-orange to-red-800">
         MASTERING INSTRUMENTS
       </span>
-      <h1 className=" mt-1 text-6xl xl:text-5xl lg:text-5xl sm:text-4xl text-center font-black text-transparent bg-clip-text bg-gradient-to-b from-stone-600 from-40%  via-stone-700 via-60% to-stone-800 to-100%  ">
+      <h1 className="  mt-1 text-6xl xl:text-5xl lg:text-5xl sm:text-4xl text-center font-black text-transparent bg-clip-text bg-gradient-to-b from-stone-600 from-40%  via-stone-700 via-60% to-stone-800 to-100%  ">
         Acquire new look
       </h1>
       <div className="relative w-screen ">
-      <div className=" mt-6 absolute flex gap-2 w-screen overflow-hidden clip-path-reverse">
-        <div className="py-12 mx-2 flex gap-2 flex-row animate-scroll whitespace-nowrap">
+      <div className=" mt-6 absolute w-full flex overflow-hidden clip-path-reverse ">
+      <div className={`absolute z-40 h-[80%] left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] w-2 bg-[#6045D6] rounded-sm ${showDivider ? "opacity-100" : "opacity-0"} transition-opacity `}></div>
+        <div className="py-12 flex gap-16 flex-row animate-scroll ">
          {renderUgly()}
         </div>
-        <div className="absolute flex flex-row gap-2 top-0 py-12 animate-scroll2 whitespace-nowrap">
+        <div className=" absolute   flex flex-row gap-16 top-0 py-12 animate-scroll2">
           {renderUgly()}
         </div>
       </div>
-      <div className=" mt-6 absolute flex gap-2 w-screen overflow-hidden clip-path">
-        <div className="py-12 mx-2 flex gap-2 flex-row animate-scroll whitespace-nowrap">
+      <div className=" mt-6 absolute flex gap-16 w-screen overflow-hidden clip-path">
+      <div  ref={dividerRef} className={`absolute z-40 h-[80%] left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] w-4 bg-[#6045D6] rounded-sm ${showDivider ? "opacity-100" : "opacity-0"} transition-opacity `}>
+
+      </div>
+
+        <div className="py-12   flex gap-16 flex-row animate-scroll">
           {renderPretty()}
         </div>
-        <div className="absolute flex flex-row gap-2 top-0 py-12 animate-scroll2 whitespace-nowrap">
+
+        <div className="absolute  flex flex-row gap-16 top-0 py-12 animate-scroll2">
           {renderPretty()}
         </div>
       </div>
-
+   
       </div>
          </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,18 +41,25 @@ const config: Config = {
           },
         },
         scroll: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(100%)' },
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(100% + 64px))'},
         },
         scroll2: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          '0%': { transform: 'translateX(calc(-100% - 64px))' },
+          '100%': { transform: 'translateX(0)' },
         },
       },
       animation: {
-        scroll2: 'scroll2 20s linear infinite',
-        scroll: 'scroll 20s linear infinite',
+        scroll2: 'scroll2 15s linear infinite',
+        scroll: 'scroll 15s linear infinite',
         text: 'text 4s linear infinite',
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(220, 120, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
       },
       colors:{
         purple:'#7359E3',
