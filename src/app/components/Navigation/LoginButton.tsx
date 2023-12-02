544838a8d8d20eee10f7d73b09a7c16e778e0688
Samuel Baran <samuel.baran98@gmail.com>
2023-12-02 08:08:24 +0100

    redirect

--- src/app/components/Navigation/LoginButton.tsx
+++ src/app/components/Navigation/LoginButton.tsx
@@ -3,7 +3,7 @@ import { SignInButton } from "@clerk/nextjs"
 export const LoginButton = () => {
 
     return (
-        <SignInButton redirectUrl="http://localhost:3000/profile">
+        <SignInButton redirectUrl="/profile">
                 <span className=" py-2 px-12 w-[250px] rounded-md  bg-purple text-white hover:cursor-pointer">
                     Log in
             </span>  
