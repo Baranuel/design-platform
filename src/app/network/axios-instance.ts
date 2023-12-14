a725dba303ef850181970c502881771bc3925086
Samuel Baran <samuel.baran98@gmail.com>
2023-12-14 15:20:54 +0100

    Update axios-instance.ts

--- src/app/network/axios-instance.ts
+++ src/app/network/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'http://https://design-platform.vercel.app/api/';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'https://design-platform.vercel.app/api/';
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl,
