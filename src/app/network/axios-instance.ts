501aa305fcfd9b23b761a6f9f911f05eb9bc1dc5
Samuel Baran <samuel.baran98@gmail.com>
2023-12-14 15:18:28 +0100

    forgot to actually update the url

--- src/app/network/axios-instance.ts
+++ src/app/network/axios-instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/' : 'http://https://design-platform.vercel.app/api/';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: baseUrl,
     timeout: 10000,
     headers: {'Content-Type': 'application/json'}
 });
